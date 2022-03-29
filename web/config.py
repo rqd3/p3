e0d957904bbd1880a6af278d75af54e01901d3e2
Sarter-Maximilian <sarter.maximilian@swm.de>
2022-03-29 09:29:16 +0200

    init c

--- web/config.py
+++ web/config.py
@@ -11,8 +11,8 @@ class BaseConfig:
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
-    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://p3servicebus.servicebus.windows.net/;SharedAccessKeyName=p3policy;SharedAccessKey=xxxx=;EntityPath=notificationqueue' #TODO: Update value
+    SECRET_KEY = 'xxxx'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://xxxx.servicebus.windows.net/;SharedAccessKeyName=xxxx;SharedAccessKey=xxxx=;EntityPath=notificationqueue' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
